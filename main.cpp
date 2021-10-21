50de9c58dc9ba4c6dc2652eee61d2be91f7952d5
Tibbe Eijsermans <92927669+NaniDaFAQ@users.noreply.github.com>
2021-10-21 12:37:57 +0000

    yes

--- main.cpp
+++ main.cpp
@@ -1,29 +1,48 @@
 #include <iostream>
 using namespace std;
 
+class life {
+  
+  public:
+    void MainMenu();
+    void clearScreen(){};
+    void randomize(){};
+    void changeParameters(){};
+    
+
+
+};
+
+
+
+void life::MainMenu() {
+  while(true){
+    char inputChar = cin.get();
+    while(inputChar != '\n') {
+      switch(inputChar) {
+      case 'C': 
+        clearScreen();
+        break;
+      case 'R': 
+        randomize();
+        break;
+      case 'P':
+        changeParameters();
+        break; 
+      case 'S':
+        exit(0);
+        break;
+      }
+    inputChar = cin.get();
+    }
+  }
+}
+
 
 
 int main() {
-  cout << "Enter a letter" << endl;
-  char inputChar = cin.get();
-  while(inputChar != '\n') {
-    switch(inputChar) {
-    case 'C': 
-      maak
-      cout << "1" << endl;
-      break;
-    case 'R': 
-      cout << "2" << endl;
-      break;
-    case 'P':
-      cout << "3" << endl;
-      break; 
-    case 'S':
-      cout << "4" << endl;
-      break;
-    }
-  inputChar = cin.get();
-  }
   
+  life Life;
+  Life.MainMenu();
   
 }
