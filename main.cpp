caa95c02644feec5a271d5fc135e44fd96a982b1
TibbeEijsermans <>
2021-10-21 12:26:31 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+
+
+int main() {
+  cout << "Enter a letter" << endl;
+  char inputChar = cin.get();
+  while(inputChar != '\n') {
+    switch(inputChar) {
+    case 'C': 
+      maak
+      cout << "1" << endl;
+      break;
+    case 'R': 
+      cout << "2" << endl;
+      break;
+    case 'P':
+      cout << "3" << endl;
+      break; 
+    case 'S':
+      cout << "4" << endl;
+      break;
+    }
+  inputChar = cin.get();
+  }
+  
+  
+}
